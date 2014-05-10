73f679ad83910d547736621bb0d476b1db4fa97a
Gustavo Lima <gustavomdcl@gmail.com>
2014-05-10 19:25:46 -0300

    Scroll e menu estao ok

--- assets/js/jquery.main.js
+++ assets/js/jquery.main.js
@@ -1,12 +1,55 @@
 /*! Main v1.00.0 | (c) 2014, 2014 | */
 
+// DOCUMENT LOAD =====================
+$(window).load(function(){
+	MobileTextResize(0);
+});
+
 // TAMANHO DA TELA ===================
 
 var document_width = 960;
 var window_width = $(window).width(); //Tamanho da tela
-$(window).resize(function(){ window_width = $(window).width(); });
+var window_height = $(window).height(); //Tamanho da tela
+$(window).resize(function(){
+	window_width = $(window).width();
+	window_height = $(window).height();
+	if(window_width>document_width){ resizeMaior(); } else { resizeMenor();	}
+	//REZISE FUNCTIONS
+	MobileTextResize(1);
+});
 
-// MENU ==============================
+// DOCUMENT SCROLL ===================
+var document_position = $(document).scrollTop();
+var desktop_anchor = $(document).scrollTop();
+var desktop_anchor_memory = $(document).scrollTop();
+$(document).scroll(function () {
+	document_position = $(this).scrollTop();
+});
+
+// MENU DESKTOP ======================
+
+//Waypoints
+$('.l-menu').waypoint('sticky');
+$('.m-bloco').each(function(){
+	$(this).waypoint(function(){
+		if(window_width > document_width + 1){
+			var elementoClasse = $(this).attr('id').split(' ');
+			$('.m-bloco.atual').removeClass('atual');
+			$(this).addClass('atual');
+			$('.l-menu .active').removeClass('active');
+			$('.l-menu a[href=#' + elementoClasse[0] + ']').addClass('active');
+		}
+	});
+});
+
+//Clique no menu e Scroll
+$('.l-menu a').click(function(e) {
+	e.preventDefault();
+	var sectionscroll = $(this).attr('href');
+	$(sectionscroll).animatescroll({scrollSpeed:2000,easing:'easeInOutBack',padding:0});
+});
+
+// MENU MOBILE =======================
 
 var openMenu = false;
 
@@ -36,21 +79,66 @@ $('.nav li').mouseleave(function () {
 	}
 });
 
+// MOBILE-TEXT =======================
+
+function MobileTextResize(e){
+	if(e==1){
+		if(window_width<document_width){//mobile
+			$('.m-text').each(function(){
+				$(this).width(window_width-20);
+				$(this).height(window_height-140);
+			});
+		} else {
+			$('.m-text').each(function(){
+				if($(this).hasClass('capa')) {
+					$(this).width(document_width);
+				} else {
+					$(this).width(640);
+				}
+				$(this).height(460);
+			});
+			if(document_position<544){
+				$('.stuck').removeClass('stuck');
+			} else {
+				$('.l-menu').addClass('stuck');
+				$('.sticky-wrapper').height($('.l-menu').height());
+			}
+		}
+	} else {
+		if(window_width<document_width){//mobile
+			$('.m-text').each(function(){
+				$(this).width(window_width-20);
+				$(this).height(window_height-140);
+			});
+		}
+	}
+}
+
 // RESIZE ============================
 
-$(window).resize(function(){
-	if(window_width>document_width){
-		//Maior ====================================================================
-		//Menu Dropdown
-		$('.l-header-bottom .nav .nav').css('display', 'none');
-		$('.icon-menu').next('ul').css('display','block');
-	} else {
-		//Menor ====================================================================
-		//Menu Dropdown
-		$('.l-header-bottom .nav .nav').css('display', 'block');
-		//Menu Mobile
-		$('.icon-menu').next('ul').css('display','none');
-		$('.icon-menu').parent('.l-container').parent('.m-menu').removeClass('open-nav');
-		openMenu = false;
+function resizeMaior(){
+	//Maior ====================================================================
+	//Menu Dropdown
+	$('.l-header-bottom .nav .nav').css('display', 'none');
+	$('.icon-menu').next('ul').css('display','block');
+	//Scroll Desktop
+	desktop_anchor_memory = $(document).scrollTop();
+	if(desktop_anchor_memory!=0 && desktop_anchor!=0 && desktop_anchor!=desktop_anchor_memory){
+		desktop_anchor = desktop_anchor_memory;
 	}
-});
+	if(desktop_anchor!=0){
+		$(document).scrollTop(desktop_anchor);
+		desktop_anchor_memory = desktop_anchor;
+	}
+	desktop_anchor = $(document).scrollTop();
+}
+
+function resizeMenor(){
+	//Menor ====================================================================
+	//Menu Dropdown
+	$('.l-header-bottom .nav .nav').css('display', 'block');
+	//Menu Mobile
+	$('.icon-menu').next('ul').css('display','none');
+	$('.icon-menu').parent('.l-container').parent('.m-menu').removeClass('open-nav');
+	openMenu = false;
+}
